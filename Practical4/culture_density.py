57d64c8335a2c561cbed16538646d320ea6f1b12
RenYixiang <yixiang.23@intl.zju.edu.cn>
2024-03-06 15:54:37 +0800

    second commit

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,7 +1,7 @@
-i = 0.05 #set initial density
-d = 1 #time in days
+i = 0.05 #set i as the initial density
+d = 1 #d represent the time in days
 print("the cell density on day"+str(d)+" is "+str(i))
-f = 0.9 #set the density threshold
+f = 0.9 #set f as the density threshold
 while i*2 <= f: #if the growing density lower than the threshold density, keep continuing the loops
     i *= 2
     d += 1
--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,9 +1,14 @@
-i =1 #served as a counter
-x = 4 
+i =1 #serve i as a counter
+x = 4 #the first number
 while i <= 5: #run five loops
     print(x)
     x = x*2+3
     i += 1
 
+#another method
+# x=4
+# for i in range(5):
+#     print(x)
+#     x=x*2+3
     
     
