9ab6e81d7064178ae3acb54f66ee97f0845ae1ad
RenYixiang <yixiang.23@intl.zju.edu.cn>
2024-03-08 10:16:37 +0800

    4

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -5,7 +5,7 @@ f = 0.9 #set f as the density threshold
 while i <= f: #if the growing density lower than the threshold density, keep continuing the loops
     i *= 2
     d += 1
-    if i > 1:
+    if i > 1:#the cell density maybe cannot beyond 1.0
         print("the cell desity on day6 is 1.0")
         break
     print("the cell density on day"+str(d)+" is "+str(i))#print the final result until the loops stop
