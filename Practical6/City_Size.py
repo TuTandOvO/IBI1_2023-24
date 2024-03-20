a528c84fd6c1653649b47afd7eda14ea56eded7c
RenYixiang <yixiang.23@intl.zju.edu.cn>
2024-03-20 15:38:07 +0800

    commit2

--- Practical6/City_Size.py
+++ Practical6/City_Size.py
@@ -1,12 +1,14 @@
-uk_cities=[0.56,0.62,0.04,9.7]
-China_cities=[0.58,8.4,29.9,22.2]
-import numpy as py
-import matplotlib.pyplot as plt
-x = ["Edinburgh","Glasgow","Stirling","London"]
-y = uk_cities
-a = ["Haining","Hangzhou","Shanghai","Beijing"]
-b = China_cities
-plt.barh(x,y)
-plt.barh(a,b)
-plt.show()
+uk_cities=[0.56,0.62,0.04,9.7]#a list of the size of UK cities
+China_cities=[0.58,8.4,29.9,22.2]#a list of the size of China cities 
+import numpy as py#import python tools 
+import matplotlib.pyplot as plt#import python tools 
+x = ["Edinburgh","Glasgow","Stirling","London"]#x serves as names of UK cities
+y = uk_cities#y serves as the size of UK cities
+a = ["Haining","Hangzhou","Shanghai","Beijing"]#a serves as names of China cities
+b = China_cities#b serves as the size of China cities
+plt.figure()
+plt.barh(x,y,autopct='%1.1f%%')#use the barh graph included in the matplotlib
+plt.figure()#show the figure in the second window
+plt.barh(a,b,autopct='%1.1f%%')
+plt.show()#show the graph in a new window
 plt.clf()
--- Practical6/Hours_of_day.py
+++ Practical6/Hours_of_day.py
@@ -1,9 +1,15 @@
-Average_day = {"Sleeping":8,"Classes":6,"Studying":3.5,"TV":2,"Music":1,"Other":3.5}
+import numpy as py #inport python tools
+import matplotlib.pyplot as plt #import python tools
+sleep = 8
+classes = 6
+study = 3.5
+tv = 2
+music = 1
+other = 3.5
+Average_day = {"Sleeping":sleep,"Classes":classes,"Studying":study,"TV":tv,"Music":music,"Other":other} #make a dictionary to record the data which can be changed by the variables
 print(Average_day)
-import numpy as py
-import matplotlib.pyplot as plt
-Hours_types = ["Sleeping","Classes","Studying","TV","Music","Other"]
-Hours = [8,6,3.5,2,1,3.5]
-plt.pie(Hours, labels = Hours_types)
-plt.show()
-plt.clf()
+Hours_types = ["Sleeping","Classes","Studying","TV","Music","Other"] #make a list of types of hours spending
+Hours = [sleep,classes,study,tv,music,other] #make a list of hours spending variables
+plt.pie(Hours, labels = Hours_types,autopct='%1.1f%%') #use pie graph that is included in the matplotlib
+plt.show() #show the graph in a new window
+plt.clf() #close the graph
