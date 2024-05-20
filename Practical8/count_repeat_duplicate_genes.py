9bbaaa523bb82742f066286552d2960a54629c4b
RenYixiang <yixiang.23@intl.zju.edu.cn>
2024-05-20 14:16:59 +0800

    5.20 commit

--- Practical8/count_repeat_duplicate_genes.py
+++ Practical8/count_repeat_duplicate_genes.py
@@ -15,64 +15,26 @@ with open(fasta_file_path, 'r') as fasta_file:
         if line.startswith('>'):
             gene_name = line
             genes_dict[gene_name] = ""
-            simplified_name = str(re.findall(r'gene:(.+?)\s',line))
         else:
             genes_dict[gene_name] += line.strip()
 genes = input('Please enter your sequences: ')
 if genes == 'GTGTGT':
     with open('GTGTGT_duplicate_genes.fa','w') as f1:  
         for gene_name, gene_sequence in genes_dict.items():
+            simplified_name = str(re.findall(r'gene:(.+?)\s',gene_name))
             count = gene_sequence.count('GTGTGT')
-            if count != 0:
+            if count != 0 and 'duplication' in gene_name:
                 f1.write(f"The repeat sequence '{'GTGTGT'}' appears {count} times in the gene '{simplified_name}'."+'\n'+ gene_sequence + '\n')
+                simplifed_name = ''
 if genes == 'GTCTGT':
     with open('GTCTGT_duplicate_genes.fa','w') as f2:
         for gene_name, gene_sequence in genes_dict.items():
+            simplified_name = str(re.findall(r'gene:(.+?)\s',gene_name))
             count = gene_sequence.count('GTCTGT')
-            if count !=0:
+            if count !=0 and 'duplication' in gene_name:
                 f2.write(f"The repeat sequence '{'GTCTGT'}' appears {count} times in the gene '{simplified_name}'."+'\n'+ gene_sequence + '\n')
+                simplified_name = ''
 
 
 
-
-
-
-
-# origin wrong version
-# import re
-# genes = input('Please enter your sequences: ')
-# s_cerevisiae = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
-# s_cerevisiae_text = s_cerevisiae.read().split('\n')
-# def prev_line(text, line):
-#     index = text.index(line)
-#     if index > 0:
-#         return text[index - 1]
-#     else:
-#         return None
-# fasta_file_1 = ''
-# fasta_file_2 = ''
-# count1 = 0
-# count2 = 0
-# if genes == 'GTGTGT':
-#     for line in s_cerevisiae_text:
-#         if 'GTGTGT' in line:
-#             if prev_line(s_cerevisiae_text,line):
-#                 gene_name1 = re.findall(r'gene:.+?\s',prev_line(s_cerevisiae_text,line))
-#                 if gene_name1:
-#                     fasta_file_1 += 'The GTGTGT ' + str(gene_name1) + '\n'
-#                     count1 += 1
-#     print(count1)
-#     with open('GTGTGT_duplicate_genes.fa','w') as f1:
-#         f1.write(fasta_file_1 + '\n')
-# if genes == 'GTCTGT':
-#     for line in s_cerevisiae_text:
-#         if 'GTCTGT' in line:
-#             if prev_line(s_cerevisiae_text,line):
-#                 gene_name2 = re.findall(r'gene:.+?\s',prev_line(s_cerevisiae_text,line))
-#                 if gene_name2:
-#                     fasta_file_2 += 'The GTCTGT ' + str(gene_name2) + '\n'
-#                     count2 += 1
-#     print(count2)
-#     with open('GTCTGT_duplicate_genes.fa','w') as f2:
-#         f2.write(fasta_file_2 + '\n')
 
